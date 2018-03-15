7dd4d61a4165105b59c68f7b5fc531647038f744
Unknown <ashley.john.flynn@gmail.com>
2018-03-16 10:10:28 +1100

    Get type and Get Subtype functions for Task and Result

--- ClusterFrac/source/Result.h
+++ ClusterFrac/source/Result.h
@@ -11,11 +11,28 @@ namespace cf
 		~Result();
 
 		/**
+		* Get the type ID of this class.
+		* This identifies the base class type during serialisation and deserialisation.
+		* This value must be unique among all classes.
+		* @returns The type ID of this class as a string.
+		*/
+		virtual std::string get_type() const { return "Result"; }
+
+		/**
+		* Get the subtype ID of this class.
+		* Pure virtual function, which identifies the polymorphic derived class type
+		* during serialisation and deserialisation.
+		* This value must be unique among all classes.
+		* @returns The subtype ID of this class as a string.
+		*/
+		virtual std::string get_subype() const = 0;
+
+		/**
 		* Return a pointer to a new result object that is a merged version
 		* of this result combined with all others in a std::vector.
 		* @param others A std::vector of pointers to the other results to merge with this one.
 		* @returns A pointer to a single new merged result.
 		*/
-		virtual Result *Merge(std::vector<Result *> others) const = 0;
+		virtual Result *merge(std::vector<Result *> others) const = 0;
 	};
 }
--- ClusterFrac/source/Task.h
+++ ClusterFrac/source/Task.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "DllExport.h"
 #include <vector>
+#include <string>
 
 namespace cf
 {
@@ -11,11 +12,28 @@ namespace cf
 		~Task();
 
 		/**
+		* Get the type ID of this class.
+		* This identifies the base class type during serialisation and deserialisation.
+		* This value must be unique among all classes.
+		* @returns The type ID of this class as a string.
+		*/
+		virtual std::string get_type() const { return "Task"; }
+
+		/**
+		* Get the subtype ID of this class.
+		* Pure virtual function, which identifies the polymorphic derived class type
+		* during serialisation and deserialisation.
+		* This value must be unique among all classes.
+		* @returns The subtype ID of this class as a string.
+		*/
+		virtual std::string get_subype() const = 0;
+
+		/**
 		* Split this task up as equally as possible in to N chunks, and return
 		* a std::vector of pointers to those split tasks.
 		* @param count Split the task into this many subtasks.
 		* @returns A std::vector of pointers to the new split tasks.
 		*/
-		virtual std::vector<Task *> Split(int count) const = 0;
+		virtual std::vector<Task *> split(int count) const = 0;
 	};
 }
