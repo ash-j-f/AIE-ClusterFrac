30ec12f4d8a7b81c5a9999d565327068b28261be
Unknown <ashley.john.flynn@gmail.com>
2018-03-17 17:45:31 +1100

    Testing

--- Benchmark/source/main.cpp
+++ Benchmark/source/main.cpp
@@ -7,12 +7,13 @@ int main()
 		BenchmarkTask *bmt2 = new BenchmarkTask();
 
 		bmt1->numbers = { 1, 2, 3 };
+		
 		bmt2->numbers = { 4, 5, 6 };
 
 		std::vector<cf::Task *> splitTasks = bmt1->split(2);
 
 		cf::WorkPacket p;
-		bmt1->serialize(p);
+		splitTasks[0]->serialize(p);
 		
 		//Determine type here.
 		std::string type;
