f5188ffab1f8a820f271317e7b36ee57689a7f98
Unknown <ashley.john.flynn@gmail.com>
2018-03-18 04:09:17 +1100

    Use consistent main.cpp name and main() to take incoming vars from console

--- Benchmark/source/main.cpp
+++ Benchmark/source/main.cpp
@@ -2,7 +2,9 @@
 #include "BenchmarkTask.h" 
 #include "BenchmarkResult.h" 
 
-int main()
+int main(int argc, //Number of strings in array argv  
+	char *argv[], //Array of command-line argument strings  
+	char *envp[]) // Array of environment variable strings  
 {
 		BenchmarkTask *bmt1 = new BenchmarkTask();
 
---
+++ ClusterFracClient/source/main.cpp
@@ -0,0 +1,5 @@
+int main(int argc, //Number of strings in array argv  
+	char *argv[], //Array of command-line argument strings  
+	char *envp[]) // Array of environment variable strings  
+{
+}
