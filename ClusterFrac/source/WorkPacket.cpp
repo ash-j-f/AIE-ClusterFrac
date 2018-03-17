a97ede57be92507b17d4ef078a0a98e5082a8ce4
Unknown <ashley.john.flynn@gmail.com>
2018-03-17 15:21:16 +1100

    Zlib compression WIP UNTESTED

--- ClusterFrac/source/WorkPacket.cpp
+++ ClusterFrac/source/WorkPacket.cpp
@@ -1,64 +1,94 @@
 #include "WorkPacket.h"
 
+cf::WorkPacket::WorkPacket()
+{
+	//Compression on by default
+	compression = true;
+}
+
+cf::WorkPacket::~WorkPacket()
+{
+}
+
 const void * cf::WorkPacket::onSend(std::size_t & size)
 {
-	// We only support data with a maximum size of
-	// an unsigned short (so the size can be sent
-	// in the first two bytes of the packet)
-	//assert(size <= 65535);
+	const void *tmpData;
 
-	// Cast the data to a bytef pointer
-	const Bytef* srcData = static_cast<const Bytef*>(getData());
+	if (compression)
+	{
+		// We only support data with a maximum size of
+		// an unsigned short (so the size can be sent
+		// in the first two bytes of the packet)
+		//assert(size <= 65535);
 
-	// Get the size of the packet to send
-	uLong srcSize = (uLong)getDataSize();
+		// Cast the data to a bytef pointer
+		const Bytef* srcData = static_cast<const Bytef*>(getData());
 
-	// Compute the size of the compressed data
-	uLong dstSize = compressBound(srcSize);
+		// Get the size of the packet to send
+		uLong srcSize = (uLong)getDataSize();
 
-	// Resize the vector to accomodate the compressed data,
-	// plus two bytes for our uncompressed size
-	oCompressionBuffer.resize(dstSize + 2);
+		// Compute the size of the compressed data
+		uLong dstSize = compressBound(srcSize);
 
-	// Take the first 8 bytes of srcSize
-	oCompressionBuffer[0] = srcSize & 0xFF;
+		// Resize the vector to accomodate the compressed data,
+		// plus two bytes for our uncompressed size
+		oCompressionBuffer.resize(dstSize + 2);
 
-	// And the second 8 bytes
-	oCompressionBuffer[1] = (srcSize >> 8) & 0xFF;
+		// Take the first 8 bytes of srcSize
+		oCompressionBuffer[0] = srcSize & 0xFF;
 
-	// Compress the data into the rest of the buffer
-	compress(oCompressionBuffer.data() + 2, &dstSize, srcData, srcSize);
+		// And the second 8 bytes
+		oCompressionBuffer[1] = (srcSize >> 8) & 0xFF;
 
-	// Set the size to the compressed size plus
-	// two bytes for the size marker
-	size = (dstSize + 2);
+		// Compress the data into the rest of the buffer
+		compress(oCompressionBuffer.data() + 2, &dstSize, srcData, srcSize);
+
+		// Set the size to the compressed size plus
+		// two bytes for the size marker
+		size = (dstSize + 2);
+
+		tmpData = oCompressionBuffer.data();
+
+	}
+	else
+	{
+		size = getDataSize();
+		tmpData = getData();
+	}
 
 	// Return data to send
-	return oCompressionBuffer.data();
+	return tmpData;
 }
 
 void cf::WorkPacket::onReceive(const void * data, std::size_t size)
 {
-	// Cast the data to Bytef*, the format zlib deals with
-	const Bytef* srcData = static_cast<const Bytef*>(data);
+	if (compression)
+	{
+		// Cast the data to Bytef*, the format zlib deals with
+		const Bytef* srcData = static_cast<const Bytef*>(data);
 
-	// Extract the uncompressed data size from the first two
-	// bytes in the packet so we can use it for the buffer
-	sf::Uint16 uncompressedSize = srcData[1] << 8 | srcData[0];
+		// Extract the uncompressed data size from the first two
+		// bytes in the packet so we can use it for the buffer
+		sf::Uint16 uncompressedSize = srcData[1] << 8 | srcData[0];
 
-	// Resize the vector to accomodate the uncompressed data
-	oCompressionBuffer.resize(uncompressedSize);
+		// Resize the vector to accomodate the uncompressed data
+		oCompressionBuffer.resize(uncompressedSize);
 
-	// Declare a variable for the destination size
-	uLong dstSize;
+		// Declare a variable for the destination size
+		uLong dstSize;
 
-	// Uncompress the data (remove the first two bytes)
-	uncompress(oCompressionBuffer.data(), &dstSize, (srcData + 2), (uLong)size - 2);
+		// Uncompress the data (remove the first two bytes)
+		uncompress(oCompressionBuffer.data(), &dstSize, (srcData + 2), (uLong)size - 2);
 
-	// Assert that the uncompressed size is the same as the
-	// size we were sent for the buffer
-	//assert(dstSize == uncompressedSize);
+		// Assert that the uncompressed size is the same as the
+		// size we were sent for the buffer
+		//assert(dstSize == uncompressedSize);
 
-	// Append it to the packet
-	append(oCompressionBuffer.data(), dstSize);
+		// Append it to the packet
+		append(oCompressionBuffer.data(), dstSize);
+	}
+	else
+	{
+		append(data, size);
+	}
 }
--- ClusterFrac/source/WorkPacket.h
+++ ClusterFrac/source/WorkPacket.h
@@ -7,9 +7,16 @@ namespace cf
 {
 	class DLL WorkPacket : public sf::Packet
 	{
+	public:
+		WorkPacket();
+		~WorkPacket();
+
+		//Is compression during network send on or off?
+		bool compression;
+
 	private:
+		std::vector<Bytef> oCompressionBuffer;
 		virtual const void* onSend(std::size_t& size);
 		virtual void onReceive(const void* data, std::size_t size);
-		std::vector<Bytef> oCompressionBuffer;
 	};
 }
