3488b93a75a9355f7808ed75e2c695ad4011998c
Ash <ashley.john.flynn@gmail.com>
2018-03-20 09:55:16 +1100

    Client test setup

--- ClusterFracClient/source/main.cpp
+++ ClusterFracClient/source/main.cpp
@@ -1,5 +1,29 @@
+#include <iostream>
+#include <SFML/Network.hpp>
+
 int main(int argc, //Number of strings in array argv  
 	char *argv[], //Array of command-line argument strings  
 	char *envp[]) // Array of environment variable strings  
 {
+
+	std::cout << "Starting ClusterFrac client." << std::endl;
+
+	const unsigned short PORT = 5000;
+	const sf::IpAddress IPADDRESS = sf::IpAddress::getLocalAddress();
+	
+	std::cout << "My IP address: " << IPADDRESS.toString() << std::endl;
+	
+	sf::TcpSocket socket;
+
+	if (socket.connect(IPADDRESS, PORT) == sf::Socket::Done)
+	{
+		std::cout << "Connected." << std::endl;
+	}
+	else
+	{
+		std::cout << "Failed." << std::endl;
+	}
+
+	system("pause");
+
 }
