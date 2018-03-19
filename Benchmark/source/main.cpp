340409c9184fb45bff7dca5cd18adcf29d6e0e3f
Ash <ashley.john.flynn@gmail.com>
2018-03-20 09:59:46 +1100

    Benchmark server and host test connect

--- Benchmark/source/main.cpp
+++ Benchmark/source/main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <ClusterFrac.h>
 #include "BenchmarkTask.h" 
 #include "BenchmarkResult.h" 
@@ -6,29 +7,46 @@ int main(int argc, //Number of strings in array argv
 	char *argv[], //Array of command-line argument strings  
 	char *envp[]) // Array of environment variable strings  
 {
-		BenchmarkTask *bmt1 = new BenchmarkTask();
 
-		for (int i = 0; i < 1000000; i++) bmt1->numbers.push_back((float)rand() / 10.0f);
+	std::cout << "Starting ClusterFrac HOST." << std::endl;
 
-		std::vector<cf::Task *> splitTasks = bmt1->split(8);
+	const unsigned short PORT = 5000;
+	const sf::IpAddress IPADDRESS = sf::IpAddress::getLocalAddress();
 
-		std::vector<cf::Result *> resultList;
-		for (auto task : splitTasks) resultList.push_back(task->run());
+	std::cout << "My IP address: " << IPADDRESS.toString() << std::endl;
 
-		cf::Result *mergedResults = resultList[0]->merge(resultList);
+	sf::TcpSocket socket;
 
-		//cf::Result *mergedResults = bmr1->merge(std::vector<cf::Result *> { bmr1, bmr2 });
+	sf::TcpListener listener;
+	listener.listen(PORT);
+	listener.accept(socket);
+	std::cout << "New client connected: " << socket.getRemoteAddress() << std::endl;
 
-		cf::WorkPacket p;
-		//splitTasks[0]->serialize(p);
-		//
-		////Determine type here.
-		//std::string type;
-		//p >> type;
-		//
-		//bmt2->deserialize(p);
+	system("pause");
 
-		bmt1->serialize(p);
+		//BenchmarkTask *bmt1 = new BenchmarkTask();
+
+		//for (int i = 0; i < 1000000; i++) bmt1->numbers.push_back((float)rand() / 10.0f);
+
+		//std::vector<cf::Task *> splitTasks = bmt1->split(8);
+
+		//std::vector<cf::Result *> resultList;
+		//for (auto task : splitTasks) resultList.push_back(task->run());
+
+		//cf::Result *mergedResults = resultList[0]->merge(resultList);
+
+		////cf::Result *mergedResults = bmr1->merge(std::vector<cf::Result *> { bmr1, bmr2 });
+
+		//cf::WorkPacket p;
+		////splitTasks[0]->serialize(p);
+		////
+		//////Determine type here.
+		////std::string type;
+		////p >> type;
+		////
+		////bmt2->deserialize(p);
+
+		//bmt1->serialize(p);
 
 	
 }
--- ClusterFracClient/source/main.cpp
+++ ClusterFracClient/source/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, //Number of strings in array argv
 	char *envp[]) // Array of environment variable strings  
 {
 
-	std::cout << "Starting ClusterFrac client." << std::endl;
+	std::cout << "Starting ClusterFrac CLIENT." << std::endl;
 
 	const unsigned short PORT = 5000;
 	const sf::IpAddress IPADDRESS = sf::IpAddress::getLocalAddress();
